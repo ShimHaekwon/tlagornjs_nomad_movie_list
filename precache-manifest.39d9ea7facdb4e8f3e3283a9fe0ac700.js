7b8cadfae98f67fdf3d8d27708c969f2b3ba2751
tlagornjs <tlagornjs@gmail.com>
2019-05-02 21:29:07 +0900

    Updates

---
+++ precache-manifest.39d9ea7facdb4e8f3e3283a9fe0ac700.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2f71a62a3582d99ed04",
+    "url": "/tlagornjs_nomad_movie_list/static/js/runtime~main.b2f71a62.js"
+  },
+  {
+    "revision": "c97730ad81aff6f58975",
+    "url": "/tlagornjs_nomad_movie_list/static/js/main.c97730ad.chunk.js"
+  },
+  {
+    "revision": "999a22de9f8e2866e588",
+    "url": "/tlagornjs_nomad_movie_list/static/js/1.999a22de.chunk.js"
+  },
+  {
+    "revision": "c97730ad81aff6f58975",
+    "url": "/tlagornjs_nomad_movie_list/static/css/main.07cd2700.chunk.css"
+  },
+  {
+    "revision": "f0f305f738fe55e8bfbe5cd1a5b7c726",
+    "url": "/tlagornjs_nomad_movie_list/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/tlagornjs_nomad_movie_list/precache-manifest.39d9ea7facdb4e8f3e3283a9fe0ac700.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/tlagornjs_nomad_movie_list/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
